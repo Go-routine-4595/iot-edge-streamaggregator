b7fbfef71d2d2d87d2ae71212f6c50c259e9f613
Christophe Buffard <christophe.buffard@gmail.com>
2026-09-04 14:21:20 -0700

    Redpanda: fixe typo for the EMQX_CLIENT_ID host-init: add the creation of /etc/nats/certs dir NATS: add TLS with leadnodes

--- init/host-init.sh
+++ init/host-init.sh
@@ -16,13 +16,20 @@ mkdir -p \
 
 # NATS
 mkdir -p "$NATS_BASE_DIR"
+mkdir -p /etc/nats
+mkdir -p /etc/nats/certs
 
+# EMQ and NATS working directroy ownership and access
 chown -R "${TARGET_UID}:${TARGET_GID}" \
   "$EMQX_BASE_DIR" "$EMQX_BASE_DIR/log" "$EMQX_BASE_DIR/etc" "$EMQX_BASE_DIR/data" "$NATS_BASE_DIR"
 chmod -R u+rwX,g+rwX \
   "$EMQX_BASE_DIR" "$EMQX_BASE_DIR/log" "$EMQX_BASE_DIR/data" "$NATS_BASE_DIR"
 chmod u+rw \
   "$EMQX_BASE_DIR/etc"
+
+# NATS Cert in host
+chmod o+r /etc/nats
+chown -R "${TARGET_UID}:${TARGET_GID}" /etc/nats
 
 ls -ld "$EMQX_BASE_DIR" "$EMQX_BASE_DIR/log" "$EMQX_BASE_DIR/etc" "$EMQX_BASE_DIR/data" "$NATS_BASE_DIR"
 echo "-------------------------- host-init exiting --------------------------"
